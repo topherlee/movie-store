08380240f203db0255078aff1179bd453766131f
topherlee <topher.lee10@gmail.com>
2022-04-25 23:08:02 +0100

    added order views

--- movie_store/views.py
+++ movie_store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from .models import Movies
+from django.contrib.auth.models import User
+from .models import Movies, LineItem, Order
 
 
 # Create your views here.
@@ -24,6 +25,20 @@ def movie_details(request, id):
         'movie':movie
     }
     return render(request, 'movie_store/movie_details.html', context)
+
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'movie_store/order_list.html', {'orders':orders})
+
+def order_details(request, id):
+    order = get_object_or_404(Order, id=id)
+    customer = order.customer
+    user = get_object_or_404(User, id=customer.pk)
+    line_items = LineItem.objects.filter(order_id=order.id)
+    context = {
+        'order':order, 'user':user, 'line_items':line_items, 'customer':customer
+    }
+    return render(request, 'movie_store/order_details.html', context)
 
 def search(request):
     context = {}
