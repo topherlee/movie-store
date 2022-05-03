9a6ef024fcd3c0d0abc41a2e05e0696c11d3890f
topherlee <topher.lee10@gmail.com>
2022-05-03 22:32:05 +0100

    attempt #2 heroku push

--- store_project/settings.py
+++ store_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'store_project.urls'
 
