01852a09d52fead9a84965aaad92fcf909d7a817
topherlee <topher.lee10@gmail.com>
2022-05-04 13:14:39 +0100

    settings fix

--- store_project/settings.py
+++ store_project/settings.py
@@ -80,6 +80,11 @@ WSGI_APPLICATION = 'store_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': db_from_env
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -90,8 +95,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+"""
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
