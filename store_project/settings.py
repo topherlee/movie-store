3e3bbc48f05be2729b8865436531d7edfb23bc32
topherlee <topher.lee10@gmail.com>
2022-05-04 13:26:27 +0100

    added pricing on movies in search and directors page

--- store_project/settings.py
+++ store_project/settings.py
@@ -81,9 +81,11 @@ WSGI_APPLICATION = 'store_project.wsgi.application'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
+"""
 DATABASES = {
     'default': db_from_env
 }
+
 """
 DATABASES = {
     'default': {
@@ -95,8 +97,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
-#DATABASES['default'].update(db_from_env)
+
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
